1f047ed81f597c4a647c1753bfb8a223187060e6
Samuel De Backer <sdebacker@gmail.com>
2015-03-16 12:11:55 +0100

    sql backup removed

--- src/Http/Controllers/AdminController.php
+++ src/Http/Controllers/AdminController.php
@@ -2,19 +2,12 @@
 namespace TypiCMS\Modules\Settings\Http\Controllers;
 
 use Cache;
-use Config;
 use Input;
-use McCool\DatabaseBackup\BackupProcedure;
-use McCool\DatabaseBackup\Dumpers\MysqlDumper;
-use McCool\DatabaseBackup\Processors\ShellProcessor;
+use Log;
 use Notification;
 use Redirect;
-use Response;
-use Symfony\Component\Process\Process;
 use TypiCMS\Http\Controllers\BaseAdminController;
-use TypiCMS\Modules\Settings\Http\Requests\FormRequest;
 use TypiCMS\Modules\Settings\Repositories\SettingInterface;
-use View;
 
 class AdminController extends BaseAdminController
 {
@@ -38,51 +31,24 @@ class AdminController extends BaseAdminController
     /**
      * Store a newly created resource in storage.
      *
-     * @return Response
+     * @return Redirect
      */
     public function store()
     {
         $data = Input::all();
         $this->repository->store($data);
         return Redirect::route('admin.settings.index');
-
     }
 
     /**
      * Clear app cache
      *
-     * @return redirect
+     * @return Redirect
      */
     public function clearCache()
     {
         Cache::flush();
         Notification::success(trans('settings::global.Cache cleared') . '.');
         return Redirect::route('admin.settings.index');
-    }
-
-    /**
-     * Backup DB
-     *
-     * @return File download
-     */
-    public function backup()
-    {
-        // DB info
-        $host = Config::get('database.connections.mysql.host');
-        $username = Config::get('database.connections.mysql.username');
-        $password = Config::get('database.connections.mysql.password');
-        $database = Config::get('database.connections.mysql.database');
-
-        // SQL file
-        $file = storage_path().'/backup/'.$database.'.sql';
-
-        // Export
-        $shellProcessor = new ShellProcessor(new Process(''));
-        $dumper = new MysqlDumper($shellProcessor, $host, 3306, $username, $password, $database, $file);
-        $backup = new BackupProcedure($dumper);
-        $backup->backup();
-
-        // DL File
-        return Response::download($file);
     }
 }
--- src/resources/views/admin/index.blade.php
+++ src/resources/views/admin/index.blade.php
@@ -23,7 +23,6 @@
     <div class="col-sm-6">
 
         <div>
-            <a href="{{ route('backup') }}" class="btn btn-default"><i class="fa fa-download"></i> {{ trans('settings::global.Backup DB') }}</a>
             <a href="{{ route('cache.clear') }}" class="btn btn-default"><span class="fa fa-trash-o"></span> {{ trans('settings::global.Clear cache') }}</a>
         </div>
 
