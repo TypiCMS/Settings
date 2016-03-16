f4cabcba03f3cb40028d22b67fe1b8810c455269
Rafael Perelló Rowley <rperellorow@gmail.com>
2016-03-16 12:04:19 +0100

    Added translation to spanish

---
+++ src/resources/lang/es/global.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'name'           => 'Configuración',
+    'settings'       => 'configuracion',
+    'System info'    => 'Información del sistema',
+    'Environment'    => 'Entorno',
+    'System locales' => 'Idiomas del sistema',
+    'Locales'        => 'Idiomas',
+    'Active locale'  => 'Idioma activo',
+    'Cache'          => 'Cache',
+    'No'             => 'No',
+    'Yes'            => 'Sí',
+    'Clear cache'    => 'Vaciar cache',
+    'Cache cleared'  => 'Cache vaciada',
+];
