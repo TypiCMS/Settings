45b154dfa813dba1a08eb5cc5588d233cfc1bb7c
Samuel De Backer <sdebacker@gmail.com>
2017-01-19 10:02:29 +0100

    array alignment removed

--- src/resources/lang/en/global.php
+++ src/resources/lang/en/global.php
@@ -1,16 +1,16 @@
 <?php
 
 return [
-    'name'           => 'Settings',
-    'settings'       => 'settings',
-    'System info'    => 'System Info',
-    'Environment'    => 'Environment',
+    'name' => 'Settings',
+    'settings' => 'settings',
+    'System info' => 'System Info',
+    'Environment' => 'Environment',
     'System locales' => 'System Locales',
-    'Locales'        => 'Locales',
-    'Active locale'  => 'Active Locale',
-    'Cache'          => 'Cache',
-    'No'             => 'No',
-    'Yes'            => 'Yes',
-    'Clear cache'    => 'Clear cache',
-    'Cache cleared'  => 'Cache cleared',
+    'Locales' => 'Locales',
+    'Active locale' => 'Active Locale',
+    'Cache' => 'Cache',
+    'No' => 'No',
+    'Yes' => 'Yes',
+    'Clear cache' => 'Clear cache',
+    'Cache cleared' => 'Cache cleared',
 ];
--- src/resources/lang/fr/global.php
+++ src/resources/lang/fr/global.php
@@ -1,16 +1,16 @@
 <?php
 
 return [
-    'name'           => 'Configuration',
-    'settings'       => 'configuration',
-    'System info'    => 'Informations système',
-    'Environment'    => 'Environnement',
+    'name' => 'Configuration',
+    'settings' => 'configuration',
+    'System info' => 'Informations système',
+    'Environment' => 'Environnement',
     'System locales' => 'Locales du système',
-    'Locales'        => 'Langues',
-    'Active locale'  => 'Langue active',
-    'Cache'          => 'Cache',
-    'No'             => 'Non',
-    'Yes'            => 'Oui',
-    'Clear cache'    => 'Vider le cache',
-    'Cache cleared'  => 'Le cache a été vidé',
+    'Locales' => 'Langues',
+    'Active locale' => 'Langue active',
+    'Cache' => 'Cache',
+    'No' => 'Non',
+    'Yes' => 'Oui',
+    'Clear cache' => 'Vider le cache',
+    'Cache cleared' => 'Le cache a été vidé',
 ];
