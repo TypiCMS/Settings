03ef7c25929927fe3a4c2fe980bbd597206e77e1
Samuel De Backer <sdebacker@gmail.com>
2018-09-29 10:54:08 +0200

    publish tag

--- src/Providers/ModuleProvider.php
+++ src/Providers/ModuleProvider.php
@@ -24,7 +24,7 @@ class ModuleProvider extends ServiceProvider
 
         $this->publishes([
             __DIR__.'/../resources/views' => resource_path('views/vendor/settings'),
-        ], 'typicms-views');
+        ], 'views');
     }
 
     public function register()
